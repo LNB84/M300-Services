3a364e6e8d31122c11b6e4b9d9d5f53c2ad4908d
Brauchli <Brl@awk.ch>
2022-03-21 15:10:21 +0100

    Changes

---
+++ lb2/AdditionalFiles/html/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css.css">
+<title>Registration</title>
+</head>
+<body>
+
+<h3>Name</h3>
+<p>This form will send the following data to the database server.</p>
+
+<?php if(isset($_GET['success']) && $_GET['success']==false): ?>
+	<p class="error">ERROR! Please try again.</p>
+<?php elseif(isset($_GET['success']) && $_GET['success']==true): ?>
+	<p class="success">Your data has been successfully submitted to the database server.</p>
+<?php endif; ?>
+
+<form method="POST" action="/process.php">
+<label>First Name:</label>
+
+<input type="text" name="first">
+<br />
+<label>Last Name:</label>
+<input type="text" name="last">
+<br />
+<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
