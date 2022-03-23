9679aa1664fed03ed95fc744198db0c8bfcdc851
LNB84 <100420669+LNB84@users.noreply.github.com>
2022-03-23 09:31:04 +0100

    Update web_shell.sh
    Unnötigen Code gelöscht.

--- lb2/web_shell.sh
+++ lb2/web_shell.sh
@@ -7,9 +7,6 @@ apt install -y apache2
 # PHP installieren
 sudo apt-get install -y php-fpm php-mysql
 sudo apt-get install -y php libapache2-mod-php php-mysql
-sudo rm -rf "/var/www/html/index.html"
-sudo cp "/AdditionalFiles/html/index.html" "/var/www/html"
-sudo cp "/AdditionalFiles/html/process.php" "/var/www/html"
 
 # Dienst neu Starten
 sudo service apache2 restart
